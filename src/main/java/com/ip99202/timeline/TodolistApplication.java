c5a2d5983d9beceba31c2a947a3eea724265b31d
ip99202 <ip99202@naver.com>
2020-11-17 01:11:46 +0900

    Fix main application

--- src/main/java/com/ip99202/timeline/TodolistApplication.java
+++ src/main/java/com/ip99202/timeline/TodolistApplication.java
@@ -1,8 +1,10 @@
-package com.ip99202.todolist;
+package com.ip99202.timeline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class TodolistApplication {
 
--- src/test/java/com/ip99202/timeline/TodolistApplicationTests.java
+++ src/test/java/com/ip99202/timeline/TodolistApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ip99202.todolist;
+package com.ip99202.timeline;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
