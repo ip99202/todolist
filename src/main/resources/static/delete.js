d0eaab13c44de9ea9ddd021169259cf538af91f9
ip99202 <ip99202@naver.com>
2020-11-18 21:08:19 +0900

    Feat frontend delete function

---
+++ src/main/resources/static/delete.js
@@ -0,0 +1,10 @@
+function deleteOne(id) {
+    $.ajax({
+        type: "DELETE",
+        url: `/api/memos/${id}`,
+        success: function (response) {
+            alert('메시지 삭제에 성공하였습니다.');
+            window.location.reload();
+        }
+    })
+}
