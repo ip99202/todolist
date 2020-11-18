97abd5236ca2dcd9a348f56805cb6cf77c6e7f0b
ip99202 <ip99202@naver.com>
2020-11-18 21:08:10 +0900

    Feat frontend edit function

---
+++ src/main/resources/static/edit.js
@@ -0,0 +1,34 @@
+function editPost(id) {
+    showEdits(id);
+    let contents = $(`#${id}-contents`).text().trim();
+    $(`#${id}-textarea`).val(contents);
+}
+
+function showEdits(id) {
+    $(`#${id}-editarea`).show();
+    $(`#${id}-submit`).show();
+    $(`#${id}-delete`).show();
+
+    $(`#${id}-contents`).hide();
+    $(`#${id}-edit`).hide();
+}
+
+function submitEdit(id) {
+    let username = $(`#${id}-username`).text().trim();
+    let contents = $(`#${id}-textarea`).val().trim();
+    if (isValidContents(contents) == false) {
+        return;
+    }
+    let data = {'name': username, 'content': contents};
+
+    $.ajax({
+        type: "PUT",
+        url: `/api/memos/${id}`,
+        contentType: "application/json",
+        data: JSON.stringify(data),
+        success: function (response) {
+            alert('메시지 변경에 성공하였습니다.');
+            window.location.reload();
+        }
+    });
+}
